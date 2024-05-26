259a69eb54eecda4ce1be851baff3a92dec2e372
Rushi Modi <rushi.modi9@gmail.com>
2024-05-27 00:33:27 +0530

    Completed some components and sections.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,12 @@ import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
@@ -43,20 +47,22 @@ const Hero = () => {
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
-          alt="show collection"
+          src={bigShoeImg}
+          alt="shoe colletion"
           width={610}
-          height={500}
+          height={502}
           className="object-contain relative z-10"
         />
-        <div>
-          {shoes.map((shoe) => (
-            <div key={shoe}>
-              <ShoeCard 
-                imgURL={shoe}
-                changeBigShowImage={() => {}}
-                bigShoeImg=""
-              />Shoe Card 
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,35 @@
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super </span>
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover unparalleled comfort and style with our Super Quality Shoes.
+          Expertly crafted from durable materials, these shoes offer premium
+          support and elegance for any occasion.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Step into excellence today and experience footwear designed to elevate
+          your every step.
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
+      </div>
+    </section>
+  );
 }
 export default SuperQuality
